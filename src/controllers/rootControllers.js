0a6a505bab99003d89043dc52155e86efdace0a1
craftz-cat <craftzcat.ss@gmail.com>
2022-08-03 19:27:34 -0700

    [ADD_FEATURES] - Add Controllers to Root `/` page

---
+++ src/controllers/rootControllers.js
@@ -0,0 +1,3 @@
+export const getRoot = (req, res) => {
+  return res.send("<h1>Welcome to API Server of the DCS Holdum</h1>");
+};
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 // Import Controllers and Middlewares
+import { getRoot } from "../controllers/rootControllers";
 
 const rootRouter = express.Router();
 
 // Routeing
-rootRouter.route("/").get();
+rootRouter.route("/").get(getRoot);
 
 export default rootRouter;
