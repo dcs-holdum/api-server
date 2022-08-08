7e9b4e343a0205952ef923ba139d5e03669e2248
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-08-07 23:02:44 -0700

    [ADD_FEATURES] - Add checking Verfiy Key step

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,21 @@
+import { httpStatusCodes } from "./lib/https-status-codes";
+
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "DCS Holdum";
   next();
 };
+
+export const checkKeyMiddleware = async (req, res, next) => {
+  const {
+    body: { verifyKey }
+  } = req;
+
+  if (!verifyKey || verifyKey !== process.env.VERIFY_KEY) {
+    return res.sendStatus(httpStatusCodes.UNAUTHORIZED);
+  } else if (verifyKey === process.env.VERIFY_KEY) {
+    next();
+  }
+
+  next();
+};
+
